3ebc59711929e536089dfc60b3558df95ba5910c
anatechRepositories <119261516+themuhdhilmi@users.noreply.github.com>
2023-10-26 18:03:28 +0800

    migration fix

--- NextJS/start-template-auth-docker/prisma/migrations/20231026100301_initial/migration.sql
+++ NextJS/start-template-auth-docker/prisma/migrations/20231026100301_initial/migration.sql
@@ -37,6 +37,7 @@ CREATE TABLE `User` (
     `email` VARCHAR(191) NULL,
     `emailVerified` DATETIME(3) NULL,
     `image` VARCHAR(191) NULL,
+    `hashedPassword` VARCHAR(191) NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
